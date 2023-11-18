68904a17463af5c02bdc13bffdfea971ea4b986c
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-11-18 16:23:04 +0100

    🧼 chore: lint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,12 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   devtools: {
-    enabled: true
+    enabled: true,
   },
   vite: {
     clearScreen: false,
     server: {
       strictPort: true,
-    }
-  }
+    },
+  },
 })
